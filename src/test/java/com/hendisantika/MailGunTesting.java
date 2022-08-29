93324ff53e6d379e1bb0f68092a667dfde32ca3a
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-29 20:49:50 +0700

    #10 - Add givenUsingJava8_whenGeneratingRandomAlphanumericString_thenCorrect Test Case :email: :mailbox: :one:

---
+++ src/test/java/com/hendisantika/MailGunTesting.java
@@ -0,0 +1,33 @@
+package com.hendisantika;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-mailgun-sample
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 8/29/22
+ * Time: 20:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class MailGunTesting {
+    @Test
+    public void givenUsingJava8_whenGeneratingRandomAlphanumericString_thenCorrect() {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 8;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        System.out.println(generatedString);
+    }
+}
